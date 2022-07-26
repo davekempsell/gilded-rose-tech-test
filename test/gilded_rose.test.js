e1c30e033e46228ce9519b9a3a59fde2ed182c7e
Dave Kempsell <dave_kempsell@hotmail.com>
2022-07-26 17:29:37 +0100

    Item class unit tested

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -1,9 +1,12 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+  describe('Item class', function() {
+    it('has a name, sellIn value, and quality value', () => {
+      const foo = new Item('foo', 0, 0)
+      expect(foo.name).toBe('foo');
+      expect(foo.sellIn).toBe(0);
+      expect(foo.quality).toBe(0);
+    })
+  })
 });
