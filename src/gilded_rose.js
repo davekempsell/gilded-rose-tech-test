ac79383c348395a9b44cef0f16f8c523f442b72b
Dave Kempsell <dave_kempsell@hotmail.com>
2022-07-28 09:52:06 +0100

    code refactored to move logic into separate private functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,34 +10,42 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
     this.items.forEach((item) => {
       if(item.name != "Sulfuras, Hand of Ragnaros") {
         item.sellIn -= 1;
-        if(item.name.includes("Backstage passes")) {
-          if(item.sellIn <= 0) {
-            item.quality = 0;
-          } else if (item.sellIn <= 5) {
-            item.quality += 3;
-          } else if (item.sellIn <= 10) {
-            item.quality += 2;
-          } else {
-            item.quality += 1;
-          }
-        } else if(item.name != "Aged Brie" ) {
-          // Conjured items requirement added
-          item.name.includes("Conjured") ? 
-            item.sellIn <= 0 ? item.quality -= 4 :item.quality -= 2
-            : item.sellIn <= 0 ? item.quality -= 2 :item.quality -= 1;
-        } else {
-          item.quality += 1;
-        }
-        item.quality < 0 ? item.quality = 0 : item.quality = item.quality
-        item.quality > 50 ? item.quality = 50 : item.quality = item.quality
+        item.name.includes("Backstage passes") ? this.#backstagePasses(item)
+          : item.name.includes("Aged Brie") ? this.#agedBrie(item)
+            : item.name.includes("Conjured") ? this.#conjured(item) : this.#normalItem(item);
+        this.#qualityLimiter(item);  
       }
     })
-
     return this.items;
+  }
+
+  // private functions to be called within updateQuality:
+  #qualityLimiter(item) {
+    item.quality < 0 ? item.quality = 0
+      : item.quality > 50 ? item.quality = 50 : item.quality = item.quality;
+  }
+
+  #normalItem(item) {
+    item.sellIn <= 0 ? item.quality -= 2 : item.quality -= 1;
+  }
+
+  #agedBrie(item) {
+    item.quality += 1;
+  }
+
+  #backstagePasses(item) {
+    item.sellIn <= 0 ? item.quality = 0
+      : item.sellIn <= 5 ? item.quality += 3
+        : item.sellIn <= 10 ? item.quality += 2 : item.quality += 1
+  }
+
+  #conjured(item) {
+    item.sellIn <= 0 ? item.quality -= 4 : item.quality -= 2;
   }
 }
 
